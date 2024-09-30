9cd1da821c70b9c87b8846955bfa93adabea4226
EltonHUEBR <eltonej4598@gmail.com>
2024-09-29 23:02:21 -0300

    relative path update

--- service-worker.js
+++ service-worker.js
@@ -2,10 +2,10 @@ self.addEventListener('install', event => {
     event.waitUntil(
       caches.open('pwa-cache').then(cache => {
         return cache.addAll([
-          '/',
-          '/index.html',
-          '/assets/css/pico.min.css',
-          '/main.js',
+          './',
+          './index.html',
+          './assets/css/pico.min.css',
+          './main.js',
         ]);
       })
     );
